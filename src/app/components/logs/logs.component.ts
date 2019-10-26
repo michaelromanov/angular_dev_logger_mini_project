0ce12b46a44407e79b65a3d9fa44afc7c414df7b
michaelromanov <michael.alexander.romanov@gmail.com>
2019-10-25 22:55:48 -0400

    oninit test

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -18,7 +18,10 @@ export class LogsComponent implements OnInit {
 
   ngOnInit() {
     this.logs = [
-      {id: '1', text: 'Generated components', date: new Date('12/26/2017 12: 54:23')}
+      {id: '1', text: 'Generated components', date: new Date('12/26/2017 12: 54:23')}, 
+      {id: '2', text: 'Generated components', date: new Date('12/26/2017 12: 54:23')}, 
+      {id: '3', text: 'Generated components', date: new Date('12/26/2017 12: 54:23')}, 
+
     ]
   }
 
