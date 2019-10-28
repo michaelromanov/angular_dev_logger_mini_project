41a550190a9540df77bf555cf8c3e28137266c38
michaelromanov <michael.alexander.romanov@gmail.com>
2019-10-27 23:53:45 -0400

    updated test

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -21,7 +21,6 @@ export class LogsComponent implements OnInit {
       {id: '1', text: 'Generated components', date: new Date('12/26/2017 12: 54:23')}, 
       {id: '2', text: 'Generated components', date: new Date('12/26/2017 12: 54:23')}, 
       {id: '3', text: 'Generated components', date: new Date('12/26/2017 12: 54:23')}, 
-
     ]
   }
 
